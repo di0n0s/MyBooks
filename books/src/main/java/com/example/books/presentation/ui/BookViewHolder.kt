ee335d2aa65aa50edad122203ec2358895102e0a
di0n0s <sfuentecarrique@gmail.com>
2022-04-27 19:56:30 +0200

    Done BookViewHolder.kt

---
+++ books/src/main/java/com/example/books/presentation/ui/BookViewHolder.kt
@@ -0,0 +1,24 @@
+package com.example.books.presentation.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.books.R
+import com.example.books.data.entity.BookListTuple
+import com.example.books.databinding.ItemBookBinding
+
+class BookViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
+    LayoutInflater.from(parent.context).inflate(
+        R.layout.item_book, parent, false
+    )
+) {
+
+    private val binding = ItemBookBinding.bind(itemView)
+
+    fun bind(book: BookListTuple?) {
+        with(binding) {
+            numberTextView.text = book?.id.toString()
+            titleTextView.text = book?.title
+        }
+    }
+}
