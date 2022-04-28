2506f03b145a0a1c0af0d1a6afdab5364ecd3dc0
di0n0s <sfuentecarrique@gmail.com>
2022-04-28 20:01:05 +0200

    Done ProgressViewHolder.kt

---
+++ books/src/main/java/com/example/books/presentation/ui/ProgressViewHolder.kt
@@ -0,0 +1,12 @@
+package com.example.books.presentation.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.books.R
+
+class ProgressViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
+    LayoutInflater.from(parent.context).inflate(
+        R.layout.item_loading, parent, false
+    )
+)
