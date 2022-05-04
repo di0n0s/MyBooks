a8081a53a26c7e15c5418e4fad19bf1cd219296b
di0n0s <sfuentecarrique@gmail.com>
2022-05-04 13:29:59 +0200

    Manage exceptions on repo

--- books/src/test/java/com/example/books/data/repository/BooksRepositoryTest.kt
+++ books/src/test/java/com/example/books/data/repository/BooksRepositoryTest.kt
@@ -61,6 +61,115 @@ class BooksRepositoryTest {
         }
 
     @Test
+    fun `GIVEN an exception from Room and list from Network WHEN getBookList is called THEN result is list from network`() =
+        runBlockingTest {
+            //GIVEN
+            val loadSize = 20
+            val list = listOf(book)
+            Mockito.`when`(roomDataSource.getBookList(loadSize)).thenThrow(exception)
+            Mockito.`when`(networkDataSource.getBookList(loadSize)).thenReturn(list)
+
+            //WHEN
+            val result = repository.getBookList(loadSize)
+
+            //THEN
+            Assert.assertEquals(1, result.size)
+
+            Mockito.verify(roomDataSource).getBookList(loadSize)
+            Mockito.verify(networkDataSource).getBookList(loadSize)
+        }
+
+    @Test
+    fun `GIVEN a list from Room and exception from Network WHEN getBookList is called THEN result is list from room`() =
+        runBlockingTest {
+            //GIVEN
+            val loadSize = 20
+            val list = listOf(book)
+            Mockito.`when`(roomDataSource.getBookList(loadSize)).thenReturn(list)
+            Mockito.`when`(networkDataSource.getBookList(loadSize - list.size)).thenThrow(exception)
+
+            //WHEN
+            val result = repository.getBookList(loadSize)
+
+            //THEN
+            Assert.assertEquals(1, result.size)
+
+            Mockito.verify(roomDataSource).getBookList(loadSize)
+            Mockito.verify(networkDataSource).getBookList(loadSize - list.size)
+        }
+
+    @Test
+    fun `GIVEN exceptions from Room and Network WHEN getBookList is called THEN result is exception from network`() =
+        runBlockingTest {
+            //GIVEN
+            val loadSize = 20
+            val exceptionFromRoom = RuntimeException("Exception from Room")
+            val exceptionFromNetwork = RuntimeException("Exception from Network")
+            Mockito.`when`(roomDataSource.getBookList(loadSize)).thenThrow(exceptionFromRoom)
+            Mockito.`when`(networkDataSource.getBookList(loadSize)).thenThrow(exceptionFromNetwork)
+
+            //WHEN
+            var result: RuntimeException? = null
+            try {
+                repository.getBookList(loadSize)
+            } catch (e: RuntimeException) {
+                result = e
+            }
+
+            //THEN
+            Assert.assertEquals(exceptionFromNetwork, result)
+
+            Mockito.verify(roomDataSource).getBookList(loadSize)
+            Mockito.verify(networkDataSource).getBookList(loadSize)
+        }
+
+    @Test
+    fun `GIVEN a empty list from Room and an exception from Network WHEN getBookList is called THEN result is exception from network`() =
+        runBlockingTest {
+            //GIVEN
+            val loadSize = 20
+            Mockito.`when`(roomDataSource.getBookList(loadSize)).thenReturn(listOf())
+            Mockito.`when`(networkDataSource.getBookList(loadSize)).thenThrow(exception)
+
+            //WHEN
+            var result: RuntimeException? = null
+            try {
+                repository.getBookList(loadSize)
+            } catch (e: RuntimeException) {
+                result = e
+            }
+
+            //THEN
+            Assert.assertEquals(exception, result)
+
+            Mockito.verify(roomDataSource).getBookList(loadSize)
+            Mockito.verify(networkDataSource).getBookList(loadSize)
+        }
+
+    @Test
+    fun `GIVEN a empty list from Network and an exception from Room WHEN getBookList is called THEN result is exception from Room`() =
+        runBlockingTest {
+            //GIVEN
+            val loadSize = 20
+            Mockito.`when`(roomDataSource.getBookList(loadSize)).thenThrow(exception)
+            Mockito.`when`(networkDataSource.getBookList(loadSize)).thenReturn(listOf())
+
+            //WHEN
+            var result: RuntimeException? = null
+            try {
+                repository.getBookList(loadSize)
+            } catch (e: RuntimeException) {
+                result = e
+            }
+
+            //THEN
+            Assert.assertEquals(exception, result)
+
+            Mockito.verify(roomDataSource).getBookList(loadSize)
+            Mockito.verify(networkDataSource).getBookList(loadSize)
+        }
+
+    @Test
     fun `GIVEN a book from Room WHEN getBookId is called THEN Network is not called`() =
         runBlockingTest {
             //GIVEN
